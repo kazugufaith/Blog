4669f7f605d7b1a6fe1071e956a924e023981140
kazugufaith <faithmugesia@gmail.com>
2019-06-03 15:30:55 +0300

    added mainroute for updating profile

--- app/main/views.py
+++ app/main/views.py
@@ -64,24 +64,24 @@ def profile(uname):
 
   return render_template('profile/profile.html',user=user)
 
-# @main.route('/user/<uname>/update',methods=['GET','POST'])
-# @login_required
-# def update_profile(uname):
-#   user = User.query.filter_by(username=uname).first()
+@main.route('/user/<uname>/update',methods=['GET','POST'])
+@login_required
+def update_profile(uname):
+  user = User.query.filter_by(username=uname).first()
 
-#   if user is None:
-#     abort (404)
-#   form = UpdateProfile()
+  if user is None:
+    abort (404)
+  form = UpdateProfile()
 
-#   if form.validate_on_submit():
-#     user.bio = form.bio.data
+  if form.validate_on_submit():
+    user.bio = form.bio.data
 
-#     db.session.add(user)
-#     db.session.commit()
+    db.session.add(user)
+    db.session.commit()
 
-#     return redirect(url_for('.profile',uname=user.username))
+    return redirect(url_for('.profile',uname=user.username))
 
-#   return render_template('profile/update.html',form =form)
+  return render_template('profile/update.html',form =form)
 
 # @main.route('/user/<uname>/update/pic',methods=['POST'])
 # @login_required
