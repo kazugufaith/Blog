55f9e2c8a0696bde57bd070eed5d61c7f262f2a3
kazugufaith <faithmugesia@gmail.com>
2019-06-03 16:38:10 +0300

    added manage.py

--- manage.py
+++ manage.py
@@ -1,26 +1,26 @@
-# from app import create_app,db
-# from flask_script import Server,Manager
-# from app.models import Blog,User
-# from flask_migrate import Migrate,MigrateCommand
+from app import create_app,db
+from flask_script import Server,Manager
+from app.models import Blog,User
+from flask_migrate import Migrate,MigrateCommand
 
-# app = create_app('development')
+app = create_app('development')
 
-# manager = Manager(app)
-# migrate = Migrate(app,db)
-# manager.add_command('db',MigrateCommand)
+manager = Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
-# manager.add_command('server',Server)
-# @manager.command
-# def test():
-#     """Run the unit tests."""
-#     import unittest
-#     tests = unittest.TestLoader().discover('tests')
-#     unittest.TextTestRunner(verbosity=2).run(tests)
+manager.add_command('server',Server)
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 
-# @manager.shell
-# def make_shell_context():
-#   return dict(app=app,db=db,Blog=Blog,User=User)
+@manager.shell
+def make_shell_context():
+  return dict(app=app,db=db,Blog=Blog,User=User)
 
-# if __name__=='__main__':
-#   manager.run()
+if __name__=='__main__':
+  manager.run()
