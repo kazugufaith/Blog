b1b626e5f1b52ff686ea3a09203c1b939c4da1b1
kazugufaith <faithmugesia@gmail.com>
2019-06-03 15:15:03 +0300

    added added a blog form on forms.py file

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,13 @@
-# from flask_wtf import FlaskForm
-# from wtforms import SelectField,TextAreaField,SubmitField,StringField
-# from wtforms.validators import Required
+from flask_wtf import FlaskForm
+from wtforms import SelectField,TextAreaField,SubmitField,StringField
+from wtforms.validators import Required
 
-# class BlogForm(FlaskForm):
-#   title = StringField('Blog title')
-#   category = SelectField("Choose Category",choices=[('Lifestyle','LifeStyle'),('Fitness','Health & Fitness'),('Trending','Trends'),('IT','Technology')])
-#   blog_post = TextAreaField('Write Blog',validators=[Required()])
-#   writer = StringField('Your alias name')
-#   submit = SubmitField('Submit')
+class BlogForm(FlaskForm):
+  title = StringField('Blog title')
+  category = SelectField("Choose Category",choices=[('Lifestyle','LifeStyle'),('Fitness','Health & Fitness'),('Trending','Trends'),('IT','Technology')])
+  blog_post = TextAreaField('Write Blog',validators=[Required()])
+  writer = StringField('Your alias name')
+  submit = SubmitField('Submit')
 
 # class UpdateProfile(FlaskForm):
 #   bio =TextAreaField('Short description about you.',validators=[Required()])
