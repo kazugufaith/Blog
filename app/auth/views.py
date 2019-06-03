51b994d9376751d26771e231191a7b84fb7cdab3
kazugufaith <faithmugesia@gmail.com>
2019-06-03 15:01:40 +0300

    added a authroute to verify password

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,24 +1,24 @@
-# from flask import render_template,redirect,url_for,flash,request
-# from . import auth
-# from .forms import RegistrationForm,LoginForm
-# from ..models import Blog,User
-# from flask_login import login_user,logout_user,login_required
-# from .. import db
-# from ..email import mail_message
+from flask import render_template,redirect,url_for,flash,request
+from . import auth
+from .forms import RegistrationForm,LoginForm
+from ..models import Blog,User
+from flask_login import login_user,logout_user,login_required
+from .. import db
+from ..email import mail_message
 
-# @auth.route('/login',methods = ['GET','POST'])
-# def login():
-#   login_form = LoginForm()
-#   if login_form.validate_on_submit():
-#         user = User.query.filter_by(email = login_form.email.data).first()
-#         if user is not None and user.verify_password(login_form.password.data):
-#             login_user(user,login_form.remember.data)
-#             return redirect(request.args.get('next') or url_for('main.index'))
+@auth.route('/login',methods = ['GET','POST'])
+def login():
+  login_form = LoginForm()
+  if login_form.validate_on_submit():
+        user = User.query.filter_by(email = login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user,login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
 
-#         flash('Invalid username or Password')
+        flash('Invalid username or Password')
 
-#   title = "Account login"
-#   return render_template('auth/login.html',title=title,login_form=login_form)
+  title = "Account login"
+  return render_template('auth/login.html',title=title,login_form=login_form)
 
 # @auth.route('/register',methods = ['GET','POST'])
 # def register():
