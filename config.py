86311538c7c429b704e4f6c2c216662bc04af432
kazugufaith <faithmugesia@gmail.com>
2019-06-03 16:37:40 +0300

    added config.py file

--- config.py
+++ config.py
@@ -1,31 +1,31 @@
-# import os
+import os
 
-# class Config:
-#   QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
-#   SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:1234@localhost/blog'
-#   SQLALCHEMY_TRACK_MODIFICATIONS = False
-#   SECRET_KEY = os.environ.get('SECRET_KEY')
-#   UPLOADED_PHOTOS_DEST ='app/static/photos'
+class Config:
+  QUOTES_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+  SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://moringa:1234@localhost/blog'
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
+  SECRET_KEY = os.environ.get('SECRET_KEY')
+  UPLOADED_PHOTOS_DEST ='app/static/photos'
 
-#   #  email configurations
-#   MAIL_SERVER = 'smtp.googlemail.com'
-#   MAIL_PORT = 587
-#   MAIL_USE_TLS = True
-#   MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-#   MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+  #  email configurations
+  MAIL_SERVER = 'smtp.googlemail.com'
+  MAIL_PORT = 587
+  MAIL_USE_TLS = True
+  MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+  MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
-# class ProdConfig(Config):
-#   pass
-#   # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+class ProdConfig(Config):
+  pass
+  # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
-# class TestConfig(Config):
-#   pass
+class TestConfig(Config):
+  pass
 
-# class DevConfig(Config):
-#   DEBUG = True
+class DevConfig(Config):
+  DEBUG = True
 
-# config_options = {
-#   'development': DevConfig,
-#   'production': ProdConfig,
-#   'test': TestConfig
-# }
+config_options = {
+  'development': DevConfig,
+  'production': ProdConfig,
+  'test': TestConfig
+}
