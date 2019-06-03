67a1b973fba6acd955d8468a598bbd4537841b66
kazugufaith <faithmugesia@gmail.com>
2019-06-03 15:04:44 +0300

    added a authroute logout for the user to logout

--- app/auth/views.py
+++ app/auth/views.py
@@ -35,8 +35,8 @@ def register():
 
   return render_template('auth/register.html',registration_form=form)
 
-# @auth.route('/logout')
-# @login_required
-# def logout():
-#   logout_user()
-#   return redirect(url_for("main.index"))
+@auth.route('/logout')
+@login_required
+def logout():
+  logout_user()
+  return redirect(url_for("main.index"))
