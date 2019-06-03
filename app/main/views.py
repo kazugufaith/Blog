142a367398f01cbbfa2385974fb6becd58f82586
kazugufaith <faithmugesia@gmail.com>
2019-06-03 15:27:29 +0300

    added mainroute for profile

--- app/main/views.py
+++ app/main/views.py
@@ -55,14 +55,14 @@ def delete_blog(id):
   return render_template('index.html')
 
 
-# @main.route('/user/<uname>')
-# def profile(uname):
-#   user = User.query.filter_by(username=uname).first()
+@main.route('/user/<uname>')
+def profile(uname):
+  user = User.query.filter_by(username=uname).first()
 
-#   if user is None:
-#     abort(404)
+  if user is None:
+    abort(404)
 
-#   return render_template('profile/profile.html',user=user)
+  return render_template('profile/profile.html',user=user)
 
 # @main.route('/user/<uname>/update',methods=['GET','POST'])
 # @login_required
