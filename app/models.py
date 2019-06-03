a16040eff5cd0f153dc9217d0a2f0666ad9b73e3
kazugufaith <faithmugesia@gmail.com>
2019-06-03 16:32:43 +0300

    define user objects

--- app/models.py
+++ app/models.py
@@ -34,31 +34,31 @@ class Blog(db.Model):
   def __repr__(self):
     return f'Blog{self.blog}'
 
-# class User(UserMixin,db.Model):
-#   '''
-#   Defining users objects
-#   '''
-#   __tablename__ = 'users'
-#   id = db.Column(db.Integer,primary_key = True)
-#   username =  db.Column(db.String(255),index = True)
-#   email =  db.Column(db.String(255),unique = True,index=True)
-#   bio = db.Column(db.String(255))
-#   profile_pic_path  = db.Column(db.String())
-#   password_secure = db.Column(db.String(255))
-#   blogs =  db.relationship('Blog',backref = 'user',lazy='dynamic')
-#   @property
-#   def password(self):
-#     raise AttributeError('You cannot read the password attribute')
+class User(UserMixin,db.Model):
+  '''
+  Defining users objects
+  '''
+  __tablename__ = 'users'
+  id = db.Column(db.Integer,primary_key = True)
+  username =  db.Column(db.String(255),index = True)
+  email =  db.Column(db.String(255),unique = True,index=True)
+  bio = db.Column(db.String(255))
+  profile_pic_path  = db.Column(db.String())
+  password_secure = db.Column(db.String(255))
+  blogs =  db.relationship('Blog',backref = 'user',lazy='dynamic')
+  @property
+  def password(self):
+    raise AttributeError('You cannot read the password attribute')
 
-#   @password.setter
-#   def password(self,password):
-#     self.password_secure = generate_password_hash(password)
+  @password.setter
+  def password(self,password):
+    self.password_secure = generate_password_hash(password)
 
-#   def verify_password(self,password):
-#     return check_password_hash(self.password_secure,password)
+  def verify_password(self,password):
+    return check_password_hash(self.password_secure,password)
 
-#   def __repr__(self):
-#     return f'User {self.username}'
+  def __repr__(self):
+    return f'User {self.username}'
 
 # class Comment(db.Model):
 #   '''
